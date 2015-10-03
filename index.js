110377db58035ec9e57474675adbbb4c41ef6402
riddler <cvdrops@outlook.com>
2015-10-03 10:53:43 +0530

    test

---
+++ index.js
@@ -0,0 +1,37 @@
+var irc = require('irc');
+var env = process.env;
+var TelegramBot = require('node-telegram-bot-api');
+
+//setting up irc configs
+var client = new irc.Client(env.server, env.nick, {
+    channels: [env.channel]
+});
+
+//setting up Telegram Bot
+var bot = new TelegramBot(env.tgbot_token, {
+    polling: true
+});
+
+
+bot.on('text', function (msg) {
+    var chatId = msg.chat.id;
+
+//    if (chatId != env.chatId) {
+//
+//        //msg from unknow telegram chat
+//        return;
+//
+//    }
+//
+//    
+//    
+//    
+//    if (!env.enabled) {
+//
+//        return;
+//    }
+
+console.log(msg);
+    
+
+});
